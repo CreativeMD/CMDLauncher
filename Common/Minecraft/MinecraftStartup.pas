1b1314379b2db2f5d94778d36a7dae7452016f74
CreativeMD <Creative-Multimedia-Design@gmx.de>
2015-11-11 14:27:32 +0100

    Fixed SCfonig needs to filled

--- Common/Minecraft/MinecraftStartup.pas
+++ Common/Minecraft/MinecraftStartup.pas
@@ -68,7 +68,7 @@ begin
       Page.AddSetting(Settings[i]);
     Group.AddPage(Page);
     Page := TSettingPage.Create('Full Config', 'Changelog.png');
-    FullServerEdit := TServerConfigEdit.Create('SConfig', 'SConfig', Instance.getLaunchSaveFile.getFileName);
+    FullServerEdit := TServerConfigEdit.Create('SConfig', 'SConfig', Instance.getLaunchSaveFile.getFileName).setNotNeedFill;
     Page.AddSetting(FullServerEdit);
     Group.AddPage(Page);
     Groups := TList<TSettingGroup>.Create;
