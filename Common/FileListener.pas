c53947f9da623188a2b13f503e795171ba0110f1
CreativeMD <Creative-Multimedia-Design@gmx.de>
2015-11-27 21:52:15 +0100

    Added internal message

---
+++ Common/FileListener.pas
@@ -0,0 +1,68 @@
+unit FileListener;
+
+interface
+
+uses Logger, System.Classes, Vcl.Forms, System.SysUtils, IdURI;
+
+type
+  TInteralCommunicator = class(TThread)
+    args : TStringList;
+    procedure Execute; override;
+    procedure onReceiveMessage;
+  end;
+
+procedure createListener;
+
+
+var
+Communicator : TInteralCommunicator;
+
+implementation
+
+uses CoreLoader, CommandUtils, StringUtils, Overview;
+
+procedure createListener;
+begin
+  Communicator := TInteralCommunicator.Create;
+  //Communicator.Start;
+end;
+
+procedure TInteralCommunicator.onReceiveMessage;
+var
+  i: Integer;
+  command : String;
+begin
+  MainLog.log('Received new interal messages');
+  MainLog.log(args);
+  for i := 0 to args.Count-1 do
+  begin
+    command := TIdURI.URLDecode(args[i]).Replace('cmdlauncher://', '');
+    if command.EndsWith('/') then
+      command := command.Substring(0, Length(command)-1);
+
+    MainLog.log(CommandUtils.processCommand(command));
+  end;
+
+end;
+
+procedure TInteralCommunicator.Execute;
+var
+args : TStringList;
+begin
+  while not Application.Terminated do
+  begin
+    if LoadedLauncher and (OverviewF <> nil) and (OverviewF.BackgroundTask <> nil) and (not Assigned(OverviewF.BackgroundTask.CurrentTask)) and FileExists(CommunicationFile) then
+    begin
+      args := TStringList.Create;
+      args.LoadFromFile(CommunicationFile);
+      DeleteFile(CommunicationFile);
+      Self.args := args;
+      Synchronize(onReceiveMessage);
+      args.Destroy;
+
+    end;
+    Sleep(1000);
+  end;
+end;
+
+end.
