c0c9b9878b8ac98f4bf5da24d78a7f60b3f493b4
CreativeMD <Creative-Multimedia-Design@gmx.de>
2015-11-27 21:51:36 +0100

    Added checkForProcess

--- Utils/FileUtils.pas
+++ Utils/FileUtils.pas
@@ -3,7 +3,7 @@ unit FileUtils;
 interface
 
 uses ProgressBar, System.SysUtils, System.IOUtils, System.Types, Winapi.Windows,
-JclFileUtils, Task;
+JclFileUtils, Task, TlHelp32, PsAPI;
 
 type
 TDeleteFolder = class(TTask)
@@ -32,10 +32,53 @@ procedure RenameFolder(OldPath, NewPath : string; ProgressBar : TCMDProgressBar 
 procedure DeleteFolder(Path : string; ProgressBar : TCMDProgressBar = nil);
 procedure CopyFolder(Path, CopyPath : string; ProgressBar : TCMDProgressBar = nil);
 function GetSizeOfFile(const Filename: string): Int64;
+function processExists(exeFileName: string): Boolean;
+function GetPathFromPID(const PID: cardinal): string;
 
 implementation
 
 uses Logger;
+
+function GetPathFromPID(const PID: cardinal): string;
+var
+  hProcess: THandle;
+  path: array[0..MAX_PATH - 1] of char;
+begin
+  hProcess := OpenProcess(PROCESS_QUERY_INFORMATION or PROCESS_VM_READ, false, PID);
+  if hProcess <> 0 then
+    try
+      if GetModuleFileNameEx(hProcess, 0, path, MAX_PATH) = 0 then
+        RaiseLastOSError;
+      result := path;
+    finally
+      CloseHandle(hProcess)
+    end
+  //else
+    //RaiseLastOSError;
+end;
+
+function processExists(exeFileName: string): Boolean;
+var
+  ContinueLoop: BOOL;
+  FSnapshotHandle: THandle;
+  FProcessEntry32: TProcessEntry32;
+begin
+  FSnapshotHandle := CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+  FProcessEntry32.dwSize := SizeOf(FProcessEntry32);
+  ContinueLoop := Process32First(FSnapshotHandle, FProcessEntry32);
+  Result := False;
+  while Integer(ContinueLoop) <> 0 do
+  begin
+   // MainLog.log(GetPathFromPID(FProcessEntry32.th32ProcessID));
+    if (FProcessEntry32.th32ProcessID <> GetCurrentProcessId) and (UpperCase(GetPathFromPID(FProcessEntry32.th32ProcessID)) =
+      UpperCase(ExeFileName)) then
+    begin
+      Result := True;
+    end;
+    ContinueLoop := Process32Next(FSnapshotHandle, FProcessEntry32);
+  end;
+  CloseHandle(FSnapshotHandle);
+end;
 
 function GetSizeOfFile(const Filename: string): Int64;
 var
